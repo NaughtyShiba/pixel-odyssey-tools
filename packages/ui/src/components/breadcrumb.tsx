1a70dd0764ed6feee844372c01f637e9a910d39e
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2024-08-15 18:27:22 +0300

    fix: icons in breadcrumbs

--- packages/ui/src/components/breadcrumb.tsx
+++ packages/ui/src/components/breadcrumb.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ChevronRightIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { ChevronRight, MoreHorizontal } from "lucide-react";
 import { Slot } from "@radix-ui/react-slot";
 
 import { cn } from "@repo/ui/lib/utils";
@@ -87,7 +87,7 @@ const BreadcrumbSeparator = ({
 		aria-hidden="true"
 		className={cn("[&>svg]:size-3.5", className)}
 		{...props}>
-		{children ?? <ChevronRightIcon />}
+		{children ?? <ChevronRight />}
 	</li>
 );
 BreadcrumbSeparator.displayName = "BreadcrumbSeparator";
@@ -101,7 +101,7 @@ const BreadcrumbEllipsis = ({
 		aria-hidden="true"
 		className={cn("flex h-9 w-9 items-center justify-center", className)}
 		{...props}>
-		<DotsHorizontalIcon className="h-4 w-4" />
+		<MoreHorizontal className="h-4 w-4" />
 		<span className="sr-only">More</span>
 	</span>
 );
