a917924878e1b0d7b0ce83821e6cc6ac3cab099a
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2024-08-17 23:51:38 +0300

    fix: typefix

--- apps/helper/src/app/editor/locations/actions.ts
+++ apps/helper/src/app/editor/locations/actions.ts
@@ -1,11 +1,13 @@
 "use server";
 
-export async function submitLocation(data: {
-	label: string;
-	enemies: string[];
-	npcs: string[];
-	items: string[];
-}) {
+export async function submitLocation(
+	data: Partial<{
+		label: string;
+		enemies: string[];
+		npcs: string[];
+		items: string[];
+	}>,
+) {
 	await new Promise((resolve) => {
 		resolve(null);
 	});
