41dba1dd05f2c1e5aa5e606fbcf056afa9f4b008
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2024-08-24 13:11:18 +0300

    build: retain node_modules

--- apps/helper/src/db/db.ts
+++ apps/helper/src/db/db.ts
@@ -2,11 +2,8 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schemas";
 
-export const migrationClient = postgres(
-	`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
-	{ max: 1 },
-);
-export const queryClient = postgres(
-	`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
-);
+const connectionString = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+export const migrationClient = postgres(connectionString, { max: 1 });
+export const queryClient = postgres(connectionString);
 export const db = drizzle(queryClient, { schema });
